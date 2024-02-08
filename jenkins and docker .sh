168452e5fd69737cfb697fd3a39c16e2ea3cade8
zeeshancli <102648128+zeeshancli@users.noreply.github.com>
2024-02-08 22:01:53 +0530

    Update jenkins and docker .sh

--- jenkins and docker .sh
+++ jenkins and docker .sh
@@ -25,3 +25,8 @@ echo \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
+#docker scout
+curl -fsSL https://raw.githubusercontent.com/docker/scout-cli/main/install.sh -o install-scout.sh
+sh install-scout.sh
+
